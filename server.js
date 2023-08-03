899cc6f3166bebae1442885e89563c873f58ffac
orange-na <d-0133@tsuyama.kosen-ac.jp>
2023-08-03 15:10:23 -0700

    ✨

--- server.js
+++ server.js
@@ -22,9 +22,9 @@ app.get('/users', (req, res) => {
 })
 
 app.post('/api', (req, res) => {
-    pool.query('INSERT INTO users(`username`) VALUES (?)', [req.body.username],(err, data) => {
+    pool.query('INSERT INTO users(username) VALUES (?)', [req.body.username],(err, data) => {
         if(err) return res.send(err);
-        return res.json('username has been created successfully!')
+        return res.json('username has been created successfully!');
     })
 })
 
